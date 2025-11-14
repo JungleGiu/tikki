b41122a922f870e5a89f21d64028223c78c38c93
Giu Eminente <giulemi210@gmail.com>
2025-11-14 19:06:12 +0100

    register form template created

--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-
+import { MatCard, MatCardTitle, MatCardHeader } from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatAnchor } from "@angular/material/button";
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [MatCard, MatCardTitle, MatCardHeader, MatFormFieldModule, MatAnchor],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
 export class Register {
 
+    registrerForm = new FormGroup({
+        teamName: new FormControl(''),
+        adminName: new FormControl(''),
+        adminEmail: new FormControl(''),
+        password: new FormControl(''),
+    }
+    );
 }
